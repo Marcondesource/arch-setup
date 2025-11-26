97e44414d364c1802136f40c2bd84da12bae0df5
Marcondesource <codeandframes@gmail.com>
2025-11-25 22:59:37 -0300

    Mudanças

--- Drives.sh
+++ Drives.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 {
-#Script de reinstalação de drives (GTX 550TI)
+#Script de instalação de drives (GTX 550TI)
+#Variaveis
+tmpdir="$(mktemp -d)"
+UTILS="base-devel git linux-lts-headers"
+check() {
+if [ "$?" -ne 0 ]; then
+echo "ERRO: $1"
+exit 1
+fi
+}
 
 #1 Habilitar o multilib e instalar o yay
 echo ">>>>>Habilitando multilib e instalando o yay"
@@ -10,68 +19,41 @@ sudo sed -i '/Include = \/etc\/pacman.d\/mirrorlist/s/^#//' /etc/pacman.conf
 
 echo "Atualizando repositorio"
 sudo pacman -Syu --noconfirm
-if [ "$?" -eq 0 ]; then
+check "Falha ao Atualizar"
 echo "Atualizado com sucesso"
-else
-echo "Erro ao Atualizar"
-exit 1
-fi
 
 echo "Instalando pacotes necessarios"
-sudo pacman -S --noconfirm base-devel git linux-lts-headers
-if [ "$?" -eq 0 ]; then
+sudo pacman -S --noconfirm $UTILS
+check "Falha ao Instalar"
 echo "Pacotes instalados"
-else
-echo "Erro ao instalar"
-exit 1
-fi
 
 echo "Instalando yay"
-cd /tmp
-pwd
-git clone https://aur.archlinux.org/yay.git
-cd yay
+git clone https://aur.archlinux.org/yay.git "$tmpdir" && cd "$tmpdir/yay"
 pwd
 makepkg -si
-if [ "$?" -eq 0 ]; then
+check "Falha ao Instalar"
 echo "Instalado com sucesso"
-else
-echo "Erro ao instalar"
-exit 1
-fi
 
 echo "Limpando Lixos"
 cd && pwd
-rm -rf /tmp/yay
+rm -rf "$tmpdir"
 
 #2 Instalando drives Nvidia
 echo "Instalando drives Nvidia"
 yay -S --noconfirm nvidia-390xx-dkms nvidia-390xx-utils nvidia-390xx-settings lib32-nvidia-390xx-utils
-if [ "$?" -eq 0 ]; then
+check "Falha ao Instalar"
 echo "Instalado com sucesso"
-else
-echo "Erro ao instalar"
-exit 1
-fi
 
 #Colocando Noveau em blacklist 
 echo "Removendo Noveau e habilitando Nvidia"
 echo "blacklist nouveau" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf > /dev/null
-if [ "$?" -eq 0 ]; then
+check "Falha"
 echo "Tudo certo"
-else
-echo "Erro"
-exit 1
-fi
 
 echo "Gerando initramfs"
 sudo mkinitcpio -P
-if [ "$?" -eq 0 ]; then
+check "Falha"
 echo "Tudo certo"
-else
-echo "Erro"
-exit 1
-fi
 
 } | tee ~/Documentos/Install_Drives-logs.txt
 
--- Setup.sh
+++ Setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-{
 #Script de pos instalação...
 #Variaveis (Edite conforme a distro ou interface desejada).
 BASE="git curl nano wget ark unzip zip redshift openssh ufw gparted dosfstools python python-pip android-tools usbutils
@@ -9,12 +8,19 @@ xfce4-settings xfce4-screenshooter xfce4-screensaver xfce4-pulseaudio-plugin xfc
 xfce4-panel xfce4-notifyd xfce4-mpc-plugin tumbler"
 LOGIN="lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings"
 LOGIN_I3="ly"
-UTILITARIOS="mpv geany"
+UTILITARIOS="mpv geany syncthing"
 OUTROS="firefox firefox-i18n-pt-br"
 I3="3-wm polybar picom lxappearance pacman-contrib polkit-gnome"
 ARQUIVOS="thunar thunar-archive-plugin thunar-media-tags-plugin thunar-shares-plugin 
-thunar-vcs-plugin thunar-volman ark unzip"
+thunar-vcs-plugin thunar-volman"
+check() {
+if [ "$?" -ne 0 ]; then
+echo "ERRO: $1"
+exit 1
+fi
+}
 
+{
 echo "Atualizando & instalando pacotes"
 sudo pacman -Syu  
 if [ "$?" -eq 0 ]; then
@@ -36,23 +42,15 @@ case $opcao in
       echo "Instalando Xfce4"
 sudo pacman -S --noconfirm $BASE $XFCE $LOGIN $ARQUIVOS $UTILITARIOS $OUTROS 
 sudo systemctl enable lightdm
-if [ "$?" -eq 0 ]; then
+check "Erro ao instalar"
 echo "Pacotes Instalados"
-else
-echo "Erro ao instalar pacotes"
-exit 1
-fi 
 ;;
    2) 
 	echo "Instalando I3-WM"
 sudo pacman -S --noconfirm $BASE $I3 $LOGIN_I3 #ARQUIVOS $UTILITARIOS $OUTROS 
 sudo systemctl enable ly
-if [ "$?" -eq 0 ]; then
+check "Erro ao instalar"
 echo "Pacotes Instalados"
-else
-echo "Erro ao instalar pacotes"
-exit 1
-fi 
 ;;
 	*)
 	  echo "Opção invalida"
@@ -63,12 +61,8 @@ esac
 #Instalando pacotes via Yay (Modifique conforme seu uso...)
 echo "Instalando pacotes via Yay"
 yay -S --noconfirm jdownloader2 freedownloadmanager vscodium  
-if [ "$?" -eq 0 ]; then
+check "Erro ao Instalar"
 echo "Pacotes instalados"
-else
-echo "Erro ao instalar pacotes"
-exit 1
-fi 
 
 #Configurando.....
 echo "Checando se o Firefox esta instalado"
@@ -82,12 +76,8 @@ fi
 echo "Configurando Firefox"
 mkdir -p ~/.config
 cp -R configs/.mozilla ~/.config
-if [ "$?" -eq 0 ]; then 
+check "Erro ao Instalar" 
 echo "Configurado"
-else
-echo "Erro"
-exit 1
-fi 
 
 echo "Checando se o Fastfetch esta instalado"
 if command -v fastfetch &> /dev/null; then
@@ -100,33 +90,21 @@ fi
 echo "Configurando Fastfetch"
 mkdir -p ~/.config/fastfetch
 cp configs/config.jsonc ~/.config/fastfetch
-if [ "$?" -eq 0 ]; then 
+check "Erro ao Configurar" 
 echo "Configurado"
-else
-echo "Erro"
-exit 1
-fi 
 
 echo "Configurando Xorg"
 sudo cp configs/xorg.conf /etc/X11 
-if [ "$?" -eq 0 ]; then 
+check "Erro ao Configurar"
 echo "Configurado"
-else
-echo "Erro"
-exit 1
-fi 
 
 echo "Configurando gtk"
 cp -R configs/gtk-3.0 ~/.config
-if [ "$?" -eq 0 ]; then 
+check "Erro ao Configurar"
 echo "Configurado"
-else
-echo "Erro"
-exit 1
-fi 
 
 echo "Checando se o VSCodium esta instalado"
-if command -v VSCodium &> /dev/null; then
+if command -v codium &> /dev/null; then
 echo "sim o VSCodium esta instalado, configurando"
 else
 echo "VSCodium não instalado"
@@ -135,11 +113,7 @@ fi
  
 # Lista das extensões do VSCodium
 EXTS=(
-  "esbenp.prettier-vscode"
   "formulahendry.code-runner"
-  "ms-python.python"
-  "timonwong.shellcheck"
-  "usernamehw.errorlens"
 )
 
 echo " Instalando extensões do VSCodium..."
@@ -147,12 +121,8 @@ for ext in "${EXTS[@]}"; do
   codium --install-extension "$ext"
 done
 
-if [ "$?" -eq 0 ]; then
+check "Erro ao Instalar"
 echo "Extensões instaladas!"
-else
-echo "Erro ao instalar extensões"
-exit 1
-fi
 
 echo "Checando se o Geany esta instalado"
 if command -v geany &> /dev/null; then
@@ -181,19 +151,15 @@ exit 1
 fi
 
 echo "Configurando kodi"
-sudo cp -R configs/.kodi/ ~/ 
-if [ "$?" -eq 0 ]; then 
+cp -R configs/.kodi/ ~/ 
+check "Erro ao Configurar"
 echo "Configurado"
-else
-echo "Erro"
-exit 1
-fi 
 
 echo "Configurandos temas"
 sudo mkdir -p /usr/share/themes
 sudo mkdir -p /usr/share/icons
 sudo mkdir -p /usr/share/backgrounds
-cd config/Themes
+cd configs/Themes
 sudo cp -R * /usr/share/themes 
 cd ../
 cd Icons/
@@ -220,12 +186,8 @@ gsettings set org.gnome.desktop.interface icon-theme "BluecurveRH"
 gsettings set org.gnome.desktop.interface cursor-theme "volantes_light_cursors"
 ;;
 esac
-if [ "$?" -eq 0 ]; then
+check "Erro ao Configurar"
 echo "Configurado"
-else
-echo "Erro ao Configurar"
-exit 1
-fi 
 
 echo "configurando cores do terminal"
 mkdir -p ~/.local/share/
@@ -234,12 +196,8 @@ export TERMINAL="xfce4-terminal"
 export GOGH_NONINTERACTIVE="true" 
 export GOGH_USE_NEW_THEME="true"
 ~/.local/share/gogh/themes/dracula.sh
-if [ "$?" -eq 0 ]; then
+check "Erro ao Configurar"
 echo "Configurado"
-else
-echo "Erro ao Configurar"
-exit 1
-fi 
 
 echo "Instalando fontes"
 FONTS=("JetBrainsMono" "Hack" "FiraCode" "Inconsolata")
@@ -248,12 +206,8 @@ for font in "${FONTS[@]}"; do
   oh-my-posh font install "$font" --user
 done
 
-if [ "$?" -eq 0 ]; then
+check "Erro ao Instalar"
 echo "Fontes instaladas"
-else 
-echo "Erro ao instalar as fontes"
-exit 1 
-fi 
 
 fc-cache -fv
 
@@ -261,7 +215,7 @@ echo "Checando se o Rofi esta instalado"
 if command -v rofi &> /dev/null; then 
 echo "sim o Rofi esta instalado, configurando"
 else
-echo "ROfi não instalado"
+echo "Rofi não instalado"
 exit 1
 fi 
 
@@ -271,12 +225,8 @@ cd configs/themes/
 cp *.rasi ~/.config/rofi/themes/
 cd ../../
 ln -sf ~/.config/rofi/themes/arc-dark.rasi ~/.config/rofi/config.rasi
-if [ "$?" -eq 0 ]; then
+check "Erro ao Configurar"
 echo "Rofi configurado"
-else
-echo "Erro ao configurar"
-exit 1
-fi
 
 echo "Checando se o Fish esta instalado"
 if command -v fish &> /dev/null; then 
@@ -288,26 +238,20 @@ fi
 
 echo "Configurando Fish"
 cp -R configs/fish/ ~/.config/
-if [ "$?" -eq 0 ]; then
+check "Erro ao Configurar"
 echo "Fish configurado"
-else
-echo "Erro ao configurar o fish"
-exit 1 
-fi  
+
+echo "Configurando Syncthing"
+systemctl --user enable --now syncthing.service
 
 echo "Configurando Firewall"
 sudo ufw enable
 sudo ufw allow 22
 sudo ufw allow ssh
 sudo ufw status  
-if [ "$?" -eq 0 ]; then
+check "Erro ao Configurar"
 echo "Firewall configurado"
-else
-echo "Erro ao configurar Firewall"
-exit 1
-fi
-} | tee ~/Documentos/Install-logs.txt
-
+} | tee $HOME/Documentos/Install-logs.txt
 echo "            Instalação Concluida  			   "
 echo "====================================="
 echo "Os logs foram salvos em ~/Documentos"
